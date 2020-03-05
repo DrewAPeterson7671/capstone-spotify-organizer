ccb43070055a9c8dc861fd4337fe86c6f2b97f7d
Student <example@example.com>
2020-03-05 11:40:36 -0800

    tried a new api token approach to the artists controller and did not return the result i needed

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,20 @@
 class ArtistsController < ApplicationController
- def index
-   if !params[:artist_name].empty?
-     @artists = RSpotify::Artist.search(params[:artist_name])
-   else
-     redirect_to root_path
-   end
- end
- def show
-   @artist = RSpotify::Artist.find(params[:id])
- end
+
+# may need to get rid of before_action and authenticate methods using rspotify prescribed approach tentatively
+
+  before_action :authenticate
+
+  def index
+    if !params[:artist_name].empty?
+      @artists = RSpotify::Artist.search(params[:artist_name])
+    else
+      redirect_to root_path
+    end
+  end
+  def show
+    @artist = RSpotify::Artist.find(params[:id])
+  end
+  def authenticate
+    RSpotify.authenticate(“user key”, “user secret”)
+  end
 end
